3d63e73ecb217acae9458cd0245d04454f220599
Jay Nolledo <jaynolleds19@gmail.com>
2023-09-26 11:54:28 -0500

    Now with dancing chicken

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello bird world!");
+        Chicken chicken1 = new Chicken();
+        chicken1.dance();
     }
 
 }
